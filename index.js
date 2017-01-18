1e4e0337617ab4d7de6eaa00a2bfe76fc081f417
Keane <drkeaned.dasilva@hotmail.com>
2017-01-18 14:03:27 -0500

    Postbacks are now receivable

--- index.js
+++ index.js
@@ -219,6 +219,28 @@ function startupMessage(recipientId,messageText) {
    console.log("Message data: ", event.message)
 }
 
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback
+  // button for Structured Messages.
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " +
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called
+  if (payload){
+    switch(payload){
+      case 'points_standing':
+        postStanding(senderID);
+        break;
+    }
+  }
+}
+
  function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
