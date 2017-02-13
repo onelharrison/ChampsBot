76c4667842a9951afdf7761571312470072a5f68
Keane <drkeaned.dasilva@hotmail.com>
2017-02-13 15:16:26 -0500

    typo

--- index.js
+++ index.js
@@ -603,7 +603,7 @@ function receivedMessage(event) {
    // Putting a stub for now, we'll expand it in the following steps
    console.log("Message data: ", event.message)
  }
-function recievePostbacks(event){
+function recievePostback(event){
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfPostback = event.timestamp;
