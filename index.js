58799bbb31bd28c534a7d5dd3d41987685f642ad
Keane <drkeaned.dasilva@hotmail.com>
2017-02-15 14:07:50 -0500

    debugging

--- index.js
+++ index.js
@@ -438,13 +438,13 @@ function topSchools(recipientId,popSchools){
   var schools = new Array()
  for (var i = 0; i < popSchools.length; i++) {
      	 db.ref('/boySchools/' + popSchools[i]).on('value',function(snapshot){
-		  var school_details = new Array()
-		  school_details[0] = snapshot.val().schoolName
-		  school_details[1] = snapshot.val().logo
-		  school_details[2] = snapshot.val().rank
-		  school_details[3] = snapshot.val().points
-      school_details[4] = snapshot.val().nickName
-		  schools.push(school_details)
+		  schools= new Array()
+		  schools[i][0] = snapshot.val().schoolName
+		  schools[i][1] = snapshot.val().logo
+		  schools[i][2] = snapshot.val().rank
+		  schools[i][3] = snapshot.val().points
+      schools[i][4] = snapshot.val().nickName
+		  //schools.push(school_details)
 	})
   }
     var messageData = {
