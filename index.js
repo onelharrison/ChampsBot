abf22fbb116de6976afadcc9fcc6ebcfb1fbf060
Keane <drkeaned.dasilva@hotmail.com>
2017-02-16 17:13:22 -0500

    debugging

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function testFeed(){
   var stream = this; // `this` is `feedparser`, which is a stream
   var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
   var item;
-
+console.log("About to Work");
   while (item = stream.read()) {
     console.log(item);
     console.log("IT WORKED!!!!!!!!!!")
